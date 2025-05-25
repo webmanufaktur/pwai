0edc493fe90086b9a935f71296ce8460c4f801f4
webmanufaktur <info@webmanufaktur.net>
2025-05-25 21:44:50 +0200

    feat: add rockmigrations actions snippets

---
+++ .windsurf/snippets/rockmigrations/actions/delete_template.php
@@ -0,0 +1,4 @@
+<?php namespace ProcessWire;
+/** @var RockMigrations $rm */
+// Docs: https://github.com/baumrock/RockMigrations/wiki/Methods#deletetemplate
+$rm->deleteTemplate('template_to_delete');
